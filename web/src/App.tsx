f6cccd85f578c0380cf9d92bc4965beaaad05a0e
Yesman Choque <62268626+YesmanChoque@users.noreply.github.com>
2020-08-10 18:24:44 -0300

    Página inicial
    Página inicial do Proffy

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Landing from './pages/Landing'
+
+import "./assets/styles/global.css"
+// jSX - JavaScript + XML
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
